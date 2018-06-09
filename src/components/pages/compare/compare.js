42d03d7196539cdd838a18c3b1e1f29d867c76ca
sergioroandrade <oi.sergioandra.de@gmail.com>
2018-06-09 02:56:39 -0300

    Structure and card list

---
+++ src/components/pages/compare/compare.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import CardList from '../../app/card-list/card-list'
+
+import './compare.scss'
+
+class Compare extends Component {
+  render() {
+   
+    return (
+      <div className="compare">
+        <CardList></CardList>
+      </div>
+    );
+  }
+}
+
+export default Compare
